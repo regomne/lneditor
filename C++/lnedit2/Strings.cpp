8ef1a71a8e5d03e945f3b77212d31b6f49e3587f
regomne <fallingsunz@gmail.com>
2013-08-30 16:45:04 +0800

    all delete

--- C++/lnedit2/Strings.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <windows.h>
-
-//the const unicode strings for asm.
-
-extern "C" 
-{
-	//extern const WCHAR szPatQuotes[]=L"(?:[（「『]|(　+))(.*)(?(1).*|[）」』])";
-	extern const WCHAR szPatQuotes[]=L"[（「『　]+(.*?)(([）」』]+)|$|(%K%P))";
-}
